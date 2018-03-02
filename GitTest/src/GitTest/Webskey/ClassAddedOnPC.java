d395b64c063389e4b976498eb523ab65b5af08f1
Webskey <Webskey@Websky>
2018-03-02 16:15:58 +0100

    Adding class from PC

---
+++ GitTest/src/GitTest/Webskey/ClassAddedOnPC.java
@@ -0,0 +1,14 @@
+package GitTest.Webskey;
+
+public class ClassAddedOnPC {
+
+	private String mrs;
+	
+	public void setMrs(String mrs) {
+		this.mrs=mrs;
+	}
+	
+	public String getMrs() {
+		return mrs;
+	}
+}
